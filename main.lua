226848ba8d6876a95b8509834eff42125eec25d1
Ivanquepro <122983182+Ivanquepro@users.noreply.github.com>
2023-02-08 18:56:32 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -27,16 +27,14 @@ local Window = Rayfield:CreateWindow({
 
 local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
 
-local Section = Main:CreateSection("Functions")
-
-local mouse = Main:CreateSlider({
+local Slider = Tab:CreateSlider({
    Name = "Speedhack",
    Range = {0, 100},
    Increment = 1,
    Suffix = "Speed",
    CurrentValue = 1,
    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(s)
-       game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+   Callback = function(s) 
+   game.Players.LocalPlayer.Humanoid.WalkSpeed = s --0 min, 100 max
    end,
 })
