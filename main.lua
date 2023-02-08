7180ae46c928c15d3fde7141c17b26130d77cf37
Ivanquepro <122983182+Ivanquepro@users.noreply.github.com>
2023-02-08 18:17:22 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,30 +1,28 @@
+if game 189707, 1730877806
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
 local Window = Rayfield:CreateWindow({
-   Name = "Bebra Hub",
-   LoadingTitle = "I monkey",
-   LoadingSubtitle = "by arangutang",
+   Name = "Bruh Hub",
+   LoadingTitle = "Pls teach me scripting",
+   LoadingSubtitle = "by Ivan",
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
       FileName = "Big Hub"
    },
    Discord = {
-      Enabled = true,
-      Invite = "discord.gg/b2tZer5s", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false,
+      Invite = "SIRIUS", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
+      RememberJoins = false -- Set this to false to make them join the discord every time they load it up
    },
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Papuas",
+      Title = "Sirius Hub",
       Subtitle = "Key System",
-      Note = "Join the discord (discord.gg/b2tZer5s)",
+      Note = "Join the discord (discord.gg/sirius)",
       FileName = "SiriusKey",
       SaveKey = true,
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = "Admin"
    }
 })
-
-local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
-
-local Section = Tab:CreateSection("Speedhack")
