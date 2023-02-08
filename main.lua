a0b2f0ea767aa952abbd82fe39b5c6d2c27ecb77
Ivanquepro <122983182+Ivanquepro@users.noreply.github.com>
2023-02-08 17:49:06 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -36,7 +36,6 @@ local Slider = Tab:CreateSlider({
    Suffix = "Speed",
    CurrentValue = 10,
    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(0, 100)
    game:GetService("Workspace").Characters.LocalPlayer.WalkSpeed = 0, 100 --min, max      
    game:GetService("Workspace").Characters.LocalPlayer.Humanoid.WalkSpeed = s
    end,
