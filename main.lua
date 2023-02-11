624efc9457d919ce3339c7da565684ca92e5ea0a
Ivanquepro <122983182+Ivanquepro@users.noreply.github.com>
2023-02-11 11:49:07 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -177,6 +177,6 @@ end)
 
 local Button = Section.Component("Button","NoFallDamage(in bus)",function()
 if game:GetService("Workspace").PlayerCharacters.gertih5555.FallDamage.Disabled = false
-then do game:GetService("Workspace").PlayerCharacters.gertih5555.FallDamage.Disabled = true
+then game:GetService("Workspace").PlayerCharacters.gertih5555.FallDamage.Disabled = true
 end
 end)
