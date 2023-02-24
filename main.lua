6086be5575e4389684d080db69bb18206e2f18c8
Ivanquepro <122983182+Ivanquepro@users.noreply.github.com>
2023-02-24 10:04:13 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet('https://garfieldscripts.xyz/ui-libs/janlib.lua'))()
 -- [Legit Tab UI] ------------------------------------------------------------------------------------------------------------------------------------------------------------
-local LegitTab = library:AddTab("Legit"); 
+local LegitTab = library:AddTab("Main"); 
 local LegitColunm1 = LegitTab:AddColumn();
-local LegitMain = LegitColunm1:AddSection("Aim Assist")
+local LegitMain = LegitColunm1:AddSection("Local Cheats")
